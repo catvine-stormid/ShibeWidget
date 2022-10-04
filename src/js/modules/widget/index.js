2845c3efae6c2de8d21607db1e0b688ebad1d92b
CatVine <catriona.vine@stormid.com>
2022-10-04 17:27:38 +0100

    Add Cat and Bird Buttons and Page Restructure

--- src/js/modules/widget/index.js
+++ src/js/modules/widget/index.js
@@ -1,34 +1,66 @@
 /* Find element to render image to and store in a variable */
 
-// const image = document.querySelector('.animalImage');
 const container = document.querySelector('.container');
 
 // /* Hit API */
 
 const shibaImage = 'https://shibe.online/api/shibes?count=1&urls=true&httpsUrls=true';
+const catImage = 'https://shibe.online/api/cats?count=1&urls=true&httpsUrls=true';
+const birdImage = 'https://shibe.online/api/birds?count=1&urls=true&httpsUrls=true';
+
 
 function createImage() {
     const image = document.createElement('img');
     container.appendChild(image);
+    return image;
 }
 
-function createShibaButton() {
+function createButton(animal) {
     const newButton = document.createElement('button');
-    newButton.innerHTML = 'New Shiba!';
+    newButton.innerHTML = `New ${animal}!`;
     newButton.classList.add('button');
-    newButton.classList.add('newShibaButton');
+    newButton.classList.add(`new${animal}Button`);
     container.appendChild(newButton);
-};
+    return newButton;
+}
 
 function init() {
-    createImage();
-    createShibaButton();
+    const image = createImage();
+    const shibaButton = createButton('Shiba');
+    const catButton = createButton('Cat');
+    const birdButton = createButton('Bird');
+
+    insertShibaToPage();
+
+    shibaButton.addEventListener('click', () => {
+        insertShibaToPage();
+    });
+    
+    catButton.addEventListener('click', () => {
+        insertCatToPage();
+    });
+    
+    birdButton.addEventListener('click', () => {
+        insertBirdToPage();
+    });
 }
 
 async function retrieveShiba() {
     const response = await fetch(shibaImage);
     const shiba = await response.json();
     return shiba;
+}
+
+async function retrieveCat() {
+    const response = await fetch(catImage);
+    const cat = await response.json();
+    return cat;
+}
+
+async function retrieveBird() {
+    const response = await fetch(birdImage);
+    const bird = await response.json();
+    return bird;
 }
 // /* Try/Catch block with an error state */
 
@@ -45,6 +77,28 @@ async function insertShibaToPage() {
     }
 }
 
+async function insertCatToPage() {
+    try {
+        const catImage = await retrieveCat();
+        const imageContainer = document.querySelector('img');
+        imageContainer.setAttribute('src', catImage);
+    } catch (err) {
+        const errorMessage = `<p>Oops! Couldn't find a cat :( </p>`;
+        container.insertAdjacentHTML('afterbegin', errorMessage);
+    }
+}
+
+async function insertBirdToPage() {
+    try {
+        const birdImage = await retrieveBird();
+        const imageContainer = document.querySelector('img');
+        imageContainer.setAttribute('src', birdImage);
+    } catch (err) {
+        const errorMessage = `<p>Oops! Couldn't find a bird :( </p>`;
+        container.insertAdjacentHTML('afterbegin', errorMessage);
+    }
+}
+
 // /* Features:
 
 // /* Set a timer and go back to hit API */
@@ -57,13 +111,4 @@ async function insertShibaToPage() {
 
 // /* Select animal type */
 
-
 init();
-
-insertShibaToPage();
-
-const shibaButton = document.querySelector('.newShibaButton');
-
-shibaButton.addEventListener('click', () => {
-    insertShibaToPage();
-});
